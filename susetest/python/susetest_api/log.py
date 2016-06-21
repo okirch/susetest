e306ae278f3d13bddc15d135908addd47e5cb0af
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-06-21 13:20:10 +0200

    fix journal order
    fix journal order

--- susetest/python/susetest_api/log.py
+++ susetest/python/susetest_api/log.py
@@ -18,9 +18,9 @@ def has_systemd(node):
 def systemd_check(node, prio_default=4):
     # jounrnalctl is strange, it behave different:
     # sometimes in early version it print date in first line sometimes not. 
-    system_journal = "LINES=`journalctl -p{0} | wc -l`; if [ $LINES -ge 2 ]; then journalctl -p{0}; exit 1; else journalctl -p{0};  exit 0; fi".format(prio)
     node.journal.beginGroup("systemd basic checks")
     for prio in range(1, prio_default):
+    	 system_journal = "LINES=`journalctl -p{0} | wc -l`; if [ $LINES -ge 2 ]; then journalctl -p{0}; exit 1; else journalctl -p{0};  exit 0; fi".format(prio)
          # systemd/journalctl return 1 when it found no log. so we have to cheat to make the inverse. 
          # ge is greater than or equal to --> >=
 	 node.journal.beginTest("check systemd logs for PRIO: {}".format(prio))
