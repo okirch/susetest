a415c2a0ce9cee6604fbc96f43754c2c80ce3d01
Dario Maiocchi <dmaiocchi@suse.com>
2016-07-26 12:30:54 +0200

    Adding priority to journal, skeleton wip

--- susetest/python/susetest_api/systemd.py
+++ susetest/python/susetest_api/systemd.py
@@ -1,40 +1,49 @@
-#! /usr/bin/python
-
 import susetest
 import suselog
-import traceback
-import sys
 
-class SYSTEMD(Exception):
-                sys.exit(1)
+class SYSTEMD_ERR(Exception):
+	pass
 
-class systemd(node):
+class systemd():
         def __init__(self, node):
                 self.node = node
                 self.systemd = self.has_systemd()
-                self.version = get_version()
+                self.version = self.get_version()
                 self.journal  = self.get_journal()
-
-        def get_version():
+		self.journal_prio = self.check_journal()
+        def get_version(self):
             status = self.node.run("rpm -q systemd", timeout=200, quiet=True)
             if status.code != 0 and status != 0:
                         raise SYSTEMD("IMPOSSIBLE TO GET SYSTEMD VERSION ON NODE " + self.node)
             version = str(status.stdout).rstrip()
             return version
 
-        def has_systemd():
+        def has_systemd(self):
             status = self.node.run("test -x /usr/bin/systemctl" , timeout=200, quiet=True)
             if status.code != 0 and status != 0:
                 # init -> skip
-                raise SYSTEMD("no systemd in node : " + self.node)
+                raise SYSTEMD_ERR("no systemd in node : " + self.node)
             return True
 
         # get the journal full for moment.
-        def get_journal():
-                status = self.node.run("journalctl -b -o json-pretty --no-pager", quiet=True, timeout=900)
+        def get_journal(self):
+		""" get full journal"""
+                status = self.node.run("journalctl -b --no-pager", quiet=True, timeout=900)
                 if status != 0 and status != 0:
-                        raise SYSTEMD("Impossible to get journal from node" + self.node)
+                        raise SYSTEMD_ERR("Impossible to get journal from node" + self.node)
                 journal = str(status.stdout)
                 return journal
 
-        # some function that manipulate the journal.
+	def get_prio(self, prio):
+		""" get prioritized journal, not full"""
+		status = self.node.run("journalctl -b -p{0} --no-pager".format(prio), quiet=True, timeout=900)
+                if status != 0 and status != 0:
+                        raise SYSTEMD_ERR("Impossible to get journal from node" + self.node)
+                journal_prio = str(status.stdout)
+                return journal_prio
+
+	def check_journal(self, prio_default=4):
+		""" check systemd journal from p1 to p3 errors. with ignore"""
+		for prio in range(0, prio_default):
+			prio_journal[prio] = get_prio(prio)
+		return prio_journal
